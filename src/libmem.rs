70acb770840862fa54c90a031686a9f4c0aff86e
DontPanicO <dontpanic.call0@gmail.com>
2023-11-11 18:32:02 +0100

    implemented different functions to read 32, 16 or 8 bit

--- src/libmem.rs
+++ src/libmem.rs
@@ -11,11 +11,29 @@ pub(crate) fn memw(src: &[u8], dest: &mut [u8], addr: usize) -> Result<(), Error
     Ok(())
 }
 
-pub(crate) fn memr(src: &[u8], addr: usize, amount: usize) -> Result<&[u8], Error> {
-    if addr + amount > src.len() {
+pub(crate) fn memr32(src: &[u8], addr: usize) -> Result<[u8; 4], Error> {
+    if addr + 4 > src.len() {
         return Err(Error::new(ErrorKind::OutOfMemory, "reading out of bounds"));
     }
-    Ok(&src[addr..addr + amount])
+    let mut r = [0u8; 4];
+    r[0..4].copy_from_slice(&src[addr..addr + 4]);
+    Ok(r)
+}
+
+pub(crate) fn memr16(src: &[u8], addr: usize) -> Result<[u8; 2], Error> {
+    if addr + 4 > src.len() {
+        return Err(Error::new(ErrorKind::OutOfMemory, "reading out of bounds"));
+    }
+    let mut r = [0u8; 2];
+    r[0..2].copy_from_slice(&src[addr..addr + 2]);
+    Ok(r)
+}
+
+pub(crate) fn memr8(src: &[u8], addr: usize) -> Result<u8, Error> {
+    if addr + 4 > src.len() {
+        return Err(Error::new(ErrorKind::OutOfMemory, "reading out of bounds"));
+    }
+    Ok(src[addr])
 }
 
 #[cfg(test)]
@@ -31,11 +49,29 @@ mod tests {
     }
 
     #[test]
-    fn test_memr() {
+    fn test_memr32() {
         let mut memory = [0u8; 1024];
         let data = "hello_world!";
         memw(data.as_bytes(), &mut memory, 0x0).unwrap();
-        let read = memr(&memory, 0x0, data.len()).unwrap();
-        assert_eq!(data.as_bytes(), read);
+        let read = memr32(&memory, 0x0).unwrap();
+        assert_eq!(&read, "hell".as_bytes());
+    }
+
+    #[test]
+    fn test_memr16() {
+        let mut memory = [0u8; 1024];
+        let data = "hello_world!";
+        memw(data.as_bytes(), &mut memory, 0x0).unwrap();
+        let read = memr16(&memory, 0x0).unwrap();
+        assert_eq!(&read, "he".as_bytes());
+    }
+
+    #[test]
+    fn test_memr8() {
+        let mut memory = [0u8; 1024];
+        let data = "hello_world!";
+        memw(data.as_bytes(), &mut memory, 0x0).unwrap();
+        let read = memr8(&memory, 0x0).unwrap();
+        assert_eq!(read, 'h' as u8);
     }
 }
